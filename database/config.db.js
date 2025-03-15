2af9323d9b6174fb0687a5fc7a13ecdcf01eb4c7
Yordan-Nadroy <solizramosyordan@gmail.com>
2025-03-14 22:16:32 -0400

    yordan
    mejora

--- database/config.db.js
+++ database/config.db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const conectarDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://<solizramosyordan>:<LyKvSmibDY05htKV>@cluster0.xxxxx.mongodb.net/<DC_SYSTEM_>?retryWrites=true&w=majority', {
+        await mongoose.connect('mongodb+srv://solizramosyordan:LyKvSmibDY05htKV@cluster0.xxxxx.mongodb.net/DC_SYSTEM_?retryWrites=true&w=majority', {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
